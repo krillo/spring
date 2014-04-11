24272f41c7abd91792579c4bdf5b6a22f6ebf67b
Kristian Erendi <kristian@reptilo.se>
2014-04-11 10:03:08 +0200

    footer

--- wp-content/themes/spring/footer.php
+++ wp-content/themes/spring/footer.php
@@ -1,15 +1,15 @@
 <footer class="row clearfix">
   <div class="col-md-3 column">
-    <address> <strong>Twitter, Inc.</strong><br /> 795 Folsom Ave, Suite 600<br /> San Francisco, CA 94107<br /> <abbr title="Phone">P:</abbr> (123) 456-7890</address>
+    <address><strong>Ansvarig utgivare: </strong> BG Nilesjö<br /> <strong>Tel: </strong> 070-226 99 95<br /><strong>Epost: </strong><a href="mailto:info@magasinspring.se">info@magasinspring.se</a></address>
   </div>
   <div class="col-md-3 column">
-    <address> <strong>Twitter, Inc.</strong><br /> 795 Folsom Ave, Suite 600<br /> San Francisco, CA 94107<br /> <abbr title="Phone">P:</abbr> (123) 456-7890</address>
+    <address> <strong>Mediavänner AB</strong><br /> Billeplatsen 1<br /> 252 23 Helsingborg<br /></address>
   </div>
-  <div class="col-md-3 column">
-    <address> <strong>Twitter, Inc.</strong><br /> 795 Folsom Ave, Suite 600<br /> San Francisco, CA 94107<br /> <abbr title="Phone">P:</abbr> (123) 456-7890</address>
+  <div class="col-md-3 column" id="footer-logo">
+    <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/media-vanner-logo.png">
   </div>
-  <div class="col-md-3 column">
-    <img src="" class="footer-img">
+  <div class="col-md-3 column" id="footer-img">
+    <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/running.png" >
   </div>
 </footer>
 </div>
