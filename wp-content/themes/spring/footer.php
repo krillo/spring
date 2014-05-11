39d9bfde1d54df25c3ed7bd6efd8b7d54093aed9
Kristian Erendi <kristian@reptilo.se>
2014-05-11 13:17:29 +0200

    footer styling

--- wp-content/themes/spring/footer.php
+++ wp-content/themes/spring/footer.php
@@ -1,14 +1,12 @@
 <footer class="row clearfix">
-  <div class="col-md-3 column">
+  <div class="col-md-4 column">
     <address><strong>Ansvarig utgivare: </strong> BG Nilesjö<br /> <strong>Tel: </strong> 070-226 99 95<br /><strong>Epost: </strong><a href="mailto:info@magasinspring.se">info@magasinspring.se</a></address>
   </div>
-  <div class="col-md-3 column">
+  <div class="col-md-4 column" id="footer-address">
+    <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/media-vanner-logo.png">
     <address> <strong>Mediavänner AB</strong><br /> Billeplatsen 1<br /> 252 23 Helsingborg<br /></address>
   </div>
-  <div class="col-md-3 column" id="footer-logo">
-    <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/media-vanner-logo.png">
-  </div>
-  <div class="col-md-3 column" id="footer-img">
+  <div class="col-md-4 column" id="footer-img">
     <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/running.png" >
   </div>
 </footer>
